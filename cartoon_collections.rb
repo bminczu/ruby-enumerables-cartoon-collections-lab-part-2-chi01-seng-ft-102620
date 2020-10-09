2ebf8e00e541c0b1660ba1e749f00924d5664c1e
adventurous-format-6947 <bminczu@gmail.com>
2020-10-09 18:44:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-end  # Use an Enumerable do to capitalize and add '!' to every element in the passed in array
+# Use an Enumerable do to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-end
-
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check

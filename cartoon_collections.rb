5b32e89898c63e343b5d30f0f37e65caf61adbdb
adventurous-format-6947 <bminczu@gmail.com>
2020-10-09 18:41:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def summon_captain_planet(planeteer_calls)
     string.concat("!")
 end
 end
+end
 
 end  # Use an Enumerable do to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results

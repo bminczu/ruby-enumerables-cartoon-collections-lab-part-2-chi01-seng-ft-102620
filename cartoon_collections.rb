a483293bed763bdd752ff7090c20202fb943950a
adventurous-format-6947 <bminczu@gmail.com>
2020-10-09 18:30:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end# Use an Enumerable to square every element in the passed in array
 
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do {|string|string.capitalize}
+  planeteer_calls.map{|string|string.capitalize}
 end  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
 end
